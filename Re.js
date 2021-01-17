6d58f10e18fba8e2f5b16083c6947cbc89d8ad86
Yukkku <76182771+Yukkku@users.noreply.github.com>
2021-01-17 17:36:08 +0900

    Update Re.js

--- Re.js
+++ Re.js
@@ -6,18 +6,18 @@ class re{
       name:'RegularExpression',
       blocks:[
         {opcode:'match',blockType:BlockType.BOOLEAN,text:'/[Re]/が[St]にマッチする',
-          arguments:{Re:{type:ArgumentType.STRING,defaultValue:"^ap+l"},
+          arguments:{Re:{type:ArgumentType.STRING,defaultValue:"^ap+l"}
                      St:{type:ArgumentType.STRING,defaultValue:"hello"}
           }},
         {opcode:'search',blockType:BlockType.REPORTER,text:'[St]中の/[Re]/の場所',
-          arguments:{Re:{type:ArgumentType.STRING,defaultValue:"p+"},
+          arguments:{Re:{type:ArgumentType.STRING,defaultValue:"p+"}
                      St:{type:ArgumentType.STRING,defaultValue:"hello"}
           }},
         {opcode:'replace',blockType:BlockType.REPORTER,text:'[St]の/[Re]/を[St2]で置き換える',
-          arguments:{Re:{type:ArgumentType.STRING,defaultValue:"^ap+l"},
-                     St1:{type:ArgumentType.STRING,defaultValue:"hello"},
+          arguments:{Re:{type:ArgumentType.STRING,defaultValue:"^ap+l"}
+                     St1:{type:ArgumentType.STRING,defaultValue:"hello"}
                      St2:{type:ArgumentType.STRING,defaultValue:" ho"}
-          }}
+          }},
       ]
     }
   }
@@ -31,4 +31,4 @@ class re{
     return args.St.replace(RegExp(args.Re,'g'),args.St2)
   }
 }
-Scratch.extensions.register(new Test())
+Scratch.extensions.register(new re())
