a0cb72c5553cf2346bd66a8356bd970eafcaf48a
Yukkku <76182771+Yukkku@users.noreply.github.com>
2021-01-17 17:29:27 +0900

    Create Re.js

---
+++ Re.js
@@ -0,0 +1,34 @@
+class re{
+  constructor(){}
+  getInfo(){
+    return{
+      id:'re',
+      name:'RegularExpression',
+      blocks:[
+        {opcode:'match',blockType:BlockType.BOOLEAN,text:'/[Re]/が[St]にマッチする',
+          arguments:{Re:{type:ArgumentType.STRING,defaultValue:"^ap+l"}
+                     St:{type:ArgumentType.STRING,defaultValue:"hello"}
+          }},
+        {opcode:'search',blockType:BlockType.REPORTER,text:'[St]中の/[Re]/の場所',
+          arguments:{Re:{type:ArgumentType.STRING,defaultValue:"p+"}
+                     St:{type:ArgumentType.STRING,defaultValue:"hello"}
+          }},
+        {opcode:'replace',blockType:BlockType.REPORTER,text:'[St]の/[Re]/を[St2]で置き換える',
+          arguments:{Re:{type:ArgumentType.STRING,defaultValue:"^ap+l"}
+                     St1:{type:ArgumentType.STRING,defaultValue:"hello"}
+                     St2:{type:ArgumentType.STRING,defaultValue:" ho"}
+          }},
+      ]
+    }
+  }
+  match(){
+    return RegExp(args.Re).test(args.St)
+  }
+  search(){
+    return args.St.search(RegExp(args.Re))
+  }
+  replace(){
+    return args.St.replace(RegExp(args.Re,'g'),args.St2)
+  }
+}
+Scratch.extensions.register(new Test())
